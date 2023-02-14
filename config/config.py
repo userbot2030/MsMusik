0df680d2a09be0028b4f0a2be6ace6692f31bfbf
DjeFaris <89874919+DjeFaris@users.noreply.github.com>
2023-02-15 01:05:37 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://xnxx.com",
+    "https://github.com/DjeFaris/RitoMusik",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
